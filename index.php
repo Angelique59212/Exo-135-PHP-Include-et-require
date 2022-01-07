ef8522e606fc2d7579016ce40823216b7177190d
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-07 09:40:09 +0100

    include et require

--- index.php
+++ index.php
@@ -2,15 +2,12 @@
 
 // Premiere ligne
 
+// inclure fichier texte.php ci dessous
+require "texte.php";
 echo "Un fichier php va être inclu à la suite de ce texte ...<br><br>";
 
-// inclure fichier texte.php ci dessous
-
-
 // Deuxieme Ligne ( inclure fichiers vars.php ci dessous )
-
-
-
+require "vars.php";
 echo "J'aime également les " . $maVar;
 
 
